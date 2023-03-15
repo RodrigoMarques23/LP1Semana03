1bedfe78a05e398eac498812425ca6557bc7db31
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-15 00:49:09 +0000

    Ex01 Solved

---
+++ ChangeString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking for input values
+            Console.WriteLine("Give me a string");
+            string s = Console.ReadLine();
+            Console.WriteLine("Give me a char");
+            char a = char.Parse(Console.ReadLine());
+
+            //Running every char in string
+            foreach (char c in s)
+            {
+                if (c != a)
+                {
+                    Console.Write(c);
+                }
+            }
+        }
+    }
+}
