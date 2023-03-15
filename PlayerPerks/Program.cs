ff9883e4b3a94680728dd40da5b1a610a3c4be1c
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-15 14:53:21 +0000

    Switch case add

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -4,6 +4,9 @@ namespace PlayerPerks
 {
     class Program
     {
+        /// <summary>
+        /// Declarate Enum
+        /// </summary>
         [Flags]
         enum Perks
         {
@@ -14,10 +17,33 @@ namespace PlayerPerks
         };
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            Perks myPerks;
+            foreach (char c in args[0])
+            {
+                switch (c)
+                {
+                    case 'w': myPerks = Perks.WaterBreathing; break;
+                    case 'a': myPerks = Perks.AutoHeal; break;
+                    case 's': myPerks = Perks.DoubleJump; break;
+                    case 'd': myPerks = Perks.Stealth; break;
+                    default: Console.WriteLine(""); return;
+                }
+            }
+            Console.WriteLine($"Player perks: {myPerks}");
+
+            /*if (args.Length == 0)//In case there is no input
             {
                 Console.WriteLine("Player has no perks");
             }
+
+            if (myPerks == Perks.Stealth | Perks.DoubleJump)
+            {
+                Console.WriteLine("Silent jumper!");
+            }
+            if (myPerks != Perks.AutoHeal)
+            {
+                Console.WriteLine("Not gonna make it!");
+            }*/
         }
     }
 }
