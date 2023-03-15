dcca9052fbf35bfc49fda6a88a5dc240bea6e120
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-03-15 14:15:06 +0000

    Flags and Args length verification

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        [Flags]
+        enum Perks
+        {
+            WaterBreathing,
+            Stealth,
+            AutoHeal,
+            DoubleJump
+        };
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Player has no perks");
+            }
+        }
+    }
+}
