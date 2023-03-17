094501781fbc0d6b33c23e730aa4621c674a219b
Butchsss <rodrigomarques23work@gmail.com>
2023-03-17 10:31:09 +0000

    Switch and foreach add

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -15,8 +15,36 @@ namespace PlayerPowers
         };
         static void Main(string[] args)
         {
+            PlayerPowers myPowers;
+            myPowers = 0;
+
             Console.WriteLine("Numero de jogadores?");
             int n = Int32.Parse(Console.ReadLine());
+            int[] players = new int[n];
+
+            foreach (int a in n)
+            {
+                Console.WriteLine("Quais sao os teus poderes?");
+                switch (c)
+                {
+                    case 'f':
+                        myPowers |= PlayerPowers.Fly;
+                        break;
+
+                    case 'x':
+                        myPowers |= PlayerPowers.XRayVision;
+                        break;
+
+                    case 's':
+                        myPowers |= PlayerPowers.SuperStrenght;
+                        break;
+
+                    default:
+                        Console.WriteLine("Unkown Power");
+
+                        return;
+                }
+            }
 
             if (((myPowers & PlayerPowers.Fly) == PlayerPowers.Fly) && ((myPowers & PlayerPowers.SuperStrenght) == PlayerPowers.SuperStrenght))
             {
