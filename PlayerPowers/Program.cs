6840f0a09c775f7c67dc33e6e8bac5d33f996807
Butchsss <rodrigomarques23work@gmail.com>
2023-03-17 10:13:43 +0000

    Declarate Enum

---
+++ PlayerPowers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayerPowers
+{
+    class Program
+    {
+        [Flags]
+
+        enum PlayerPowers
+        {
+            Fly = 1,
+            XRayVision = 2,
+            SuperStrenght = 4
+
+        };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
